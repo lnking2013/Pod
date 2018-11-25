db8a77fb2ed90ddea5116397dfd7c2d072a39c47
l_ning <l_ning@Ctrip.com>
2018-11-25 09:37:37 +0800

    private lib

---
+++ PrivateLib/PrivateLib/PrivateLib.h
@@ -0,0 +1,13 @@
+//
+//  PrivateLib.h
+//  PrivateLib
+//
+//  Created by LiNing on 2018/11/23.
+//  Copyright © 2018年 LiNing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrivateLib : NSObject
+- (void)printName;
+@end
---
+++ PrivateLib/PrivateLib/PrivateLib.m
@@ -0,0 +1,16 @@
+//
+//  PrivateLib.m
+//  PrivateLib
+//
+//  Created by LiNing on 2018/11/23.
+//  Copyright © 2018年 LiNing. All rights reserved.
+//
+
+#import "PrivateLib.h"
+
+@implementation PrivateLib
+- (void)printName
+{
+    NSLog(@"I'm a private Pod lib");
+}
+@end
